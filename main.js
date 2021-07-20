2c0a55bf00af74ab4e25c9afdc7f5aeee52e3e06
Henrique Rosemback <henriquedevsantos@gmail.com>
2021-07-20 17:34:46 -0300

    concluído

--- main.js
+++ main.js
@@ -18,7 +18,6 @@ for (const link of links) {
 function changeHeaderWhenScroll() {
   const header = document.querySelector('#header')
   const navHeight = header.offsetHeight
-
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -33,7 +32,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWapperSize: true
+    }
+  }
 })
 
 /* scroll reveal efeitos na pagina quando der o scroll */
@@ -53,7 +58,6 @@ scrollReveal.reveal(
 /***** botão voltar ao topo  ******/
 function backToTopButton() {
   const backToTopButton = document.querySelector('.back-to-top')
-
   if (this.window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
@@ -61,8 +65,34 @@ function backToTopButton() {
   }
 }
 
+/* menu ativo conforme a seção visivel na página */
+function activateMenuAtCurrentSection() {
+  const sections = document.querySelectorAll('main section[id]')
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 /****** When Scroll ******/
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTopButton()
+  activateMenuAtCurrentSection()
 })
